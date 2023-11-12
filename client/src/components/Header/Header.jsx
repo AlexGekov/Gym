b23b9ed73e47ddabae52b30064e413720df2bada
AlexGekov <soter69420@gmail.com>
2023-11-12 19:31:16 +0200

    I think I finally fixed light/dark theme

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import Links from "./Links"
+import { Link } from "react-router-dom"
 
 import "./Header.css"
 
@@ -14,43 +15,6 @@ export default function Header({isAuth, setIsAuth}) {
         let darkMode = sessionStorage.setItem("darkMode", "null")
         const darkModeToggle = document.getElementsByClassName("switch")[0]
 
-        const enableDarkMode = () => {
-            let elements = document.getElementsByClassName("light")
-            document.body.classList.add("darkmode")
-            sessionStorage.setItem("darkMode", "enabled")
-            elements[0].classList.add("dark")
-            document.getElementsByTagName("a")[0].classList.remove("light")
-            elements[0].classList.add("dark")
-            document.getElementsByTagName("a")[1].classList.remove("light")
-            elements[0].classList.add("dark")
-            if (document.getElementsByClassName("hidden")[0]) {
-                document.getElementsByClassName("hidden")[0].children[0].classList.remove("light")
-                elements[0].classList.add("dark")
-                document.getElementsByClassName("hidden")[1].children[0].classList.remove("light")
-                elements[0].classList.add("dark")
-                document.getElementsByClassName("hidden")[7].children[0].classList.remove("light")
-            }
-        }
-
-
-        const disabledDarkMode = () => {
-            let elements = document.getElementsByClassName("dark")
-            document.body.classList.remove("darkmode")
-            sessionStorage.setItem("darkMode", "null")
-            elements[0].classList.remove("dark")
-            document.getElementsByTagName("a")[0].classList.add("light")
-            elements[0].classList.remove("dark")
-            document.getElementsByTagName("a")[1].classList.add("light")
-            elements[0].classList.remove("dark")
-            if (document.getElementsByClassName("hidden")[0]) {
-                document.getElementsByClassName("hidden")[0].children[0].classList.add("light")
-                elements[0].classList.remove("dark")
-                document.getElementsByClassName("hidden")[1].children[0].classList.add("light")
-                elements[0].classList.remove("dark")
-                document.getElementsByClassName("hidden")[7].children[0].classList.add("light")
-            }
-        }
-
         darkModeToggle.addEventListener("click", () => {
             darkMode = sessionStorage.getItem("darkMode")
             if (darkMode !== "enabled") {
@@ -59,12 +23,33 @@ export default function Header({isAuth, setIsAuth}) {
                 disabledDarkMode()
             }
         })
-    })
+    },[])
+
+    const enableDarkMode = () => {
+        let elements = document.getElementsByClassName("light")
+        document.body.classList.add("darkmode")
+        sessionStorage.setItem("darkMode", "enabled")
+        for (let el of elements) {
+            el.classList.add("dark")
+            el.classList.remove("ligth")
+        }
+    }
+
+
+    const disabledDarkMode = () => {
+        let elements = document.getElementsByClassName("light")
+        document.body.classList.remove("darkmode")
+        sessionStorage.setItem("darkMode", "null")
+        for (let el of elements) {
+            el.classList.add("light")
+            el.classList.remove("dark")
+        }
+    }
 
 
     return (
         <header id="header">
-            <img className="img" src="/images/429d631659a11a9eb666b103d811470a.png" alt="logo"></img>
+            <Link to="/" ><img className="img" src="/images/429d631659a11a9eb666b103d811470a.png" alt="logo"></img></Link>
             <nav id="navbar">
             <Links isAuth={isAuth} setIsAuth={setIsAuth}></Links>
             </nav>
